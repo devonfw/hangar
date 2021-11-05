341db750f58b95950f146319cd68a36c37d4e1aa
Padma <prathibha.padma@capgemini.com>
2021-11-05 10:03:06 +0530

    files updated

---
+++ Scripts/account-setup/account_create.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install the Azure DevOps extension for cli use
+az extension add --name azure-devops
+
+# Login to Azure Devops With PAT Token
+echo "You can Login by using PAT."
+cat pat_token.txt | az devops login
+
+#cat pat_token.txt | az devops login --organization https://dev.azure.com/Orgalization-org/
+
+# Configured the Organizations and project
+echo "Configure Organizations and project"
+
+az devops configure --defaults organization=https://dev.azure.com/{{organization-org}}
+
+az devops configure --defaults project={{project name}}
+
+
+# List of Configured Organizations
+echo "Configure list"
+
+az devops configure --list
