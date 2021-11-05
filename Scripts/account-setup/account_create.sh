1dc6f440d1fd4ecbb2b886af3d4effd6742d536f
prathibhapadma <90541284+prathibhapadma@users.noreply.github.com>
2021-11-05 17:39:00 +0530

    Update account_create.sh

--- Scripts/account-setup/account_create.sh
+++ Scripts/account-setup/account_create.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+organization=$1
+project=$2
+
+
 # Install the Azure DevOps extension for cli use
 az extension add --name azure-devops
 
@@ -12,9 +16,9 @@ cat pat_token.txt | az devops login
 # Configured the Organizations and project
 echo "Configure Organizations and project"
 
-az devops configure --defaults organization=https://dev.azure.com/{{organization-org}}
+az devops configure --defaults organization=https://dev.azure.com/"$organization" #prathibhapadma-org
 
-az devops configure --defaults project={{project name}}
+az devops configure --defaults project="$project"
 
 
 # List of Configured Organizations
