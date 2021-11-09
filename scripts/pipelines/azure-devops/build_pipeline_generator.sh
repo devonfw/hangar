876031066a2880f891d421ab15040a504e444412
jbellver99 <joan.bellver-fernandez@capgemini.com>
2021-11-09 11:21:38 +0100

    Script error resolved

--- scripts/pipelines/azure-devops/build_pipeline_generator.sh
+++ scripts/pipelines/azure-devops/build_pipeline_generator.sh
@@ -10,28 +10,28 @@ done
 
 if test "$1" = "-h"
 then
-    echo "Generates a build pipeline on Azure Devops"
+    echo "Generates a build pipeline on Azure DevOps"
     echo ""
     echo "Arguments:"
-    echo "  -n     [Required] Name that will be set to the build pipeline."
-    echo "  -r     [Required] URL of the repository where the code is located."
-    echo "  -b     [Required] Name of the branch for which the pipeline will be configured."
-    echo "  -y                Path to the yaml file (inside of the repository)."
+    echo "  -n    [Required] Name that will be set to the build pipeline."
+    echo "  -r    [Required] URL of the repository where the code is located."
+    echo "  -b    [Required] Name of the branch for which the pipelinewill be configured."
+    echo "  -y               Path to the yaml file (inside of the repository)"
     exit
 fi
 
 if test -z "$name" || test -z "$repository" || test -z "$branch"
 then
-    echo "Missing parameters, the flags -n -r and -b are mandatory"
+    echo "Missing parameters, the flgas -n -r and -b are mandatory."
     echo "Use -h flag to display help."
     exit
 fi
 
 if test -z "$yamlPath"
 then
-    echo "Generating build pipeline from scratch."
+    echo "Generating build pipeline from scratch..."
     az pipelines create --name $name --repository $repository --branch $branch
 else
-    echo "Generating build pipeline from the given yaml file."
+    echo "Generating build pipelie from the given yaml file..."
     az pipelines create --name $name --repository $repository --branch $branch --yaml-path $yamlPath
 fi
