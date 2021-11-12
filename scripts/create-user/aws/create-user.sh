a3250b7bcdc46916ae63aa8e561898a94109be5a
Miguel A. Cerver <miguel-angel.cerver-copovi@capgemini.com>
2021-11-12 14:58:54 +0100

    Block2: Documentation and script finished

---
+++ scripts/create-user/aws/create-user.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+while getopts u:g: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        g) groupname=${OPTARG};;
+    esac
+done
+
+if [ "$1" == "-h" ];
+then
+    echo "Creates a new user which is added to a new group attached to the necessary permissions to deploy on AWS EKS"
+    echo ""
+    echo "Arguments:"
+    echo "  -u     [Required] Username for the new user."
+    echo "  -g     [Required] Group name for the new group."
+    exit
+fi
+
+#Argument check
+if [ -z "$username" ] || [ -z "$groupname" ];
+then
+    echo "Missing parameters, the flags -u and -g are mandatory"
+    echo "Use -h flag to display help."
+    exit
+fi
+
+#AWS credentials configure
+echo "Introduce your AWS credentials"
+aws configure
+
+#Create user
+echo "Creating new user "$username" .."
+aws iam create-user --user-name $username > /dev/null
+
+#Create access key data and store to variables
+echo "Creating access key.."
+output=$(aws iam create-access-key --user-name $username)
+accessKeyId=$(echo "$output" | python -c  "import sys, json; print(json.load(sys.stdin)['AccessKey']['AccessKeyId'])")
+secretAccessKey=$(echo "$output" | python -c  "import sys, json; print(json.load(sys.stdin)['AccessKey']['SecretAccessKey'])")
+
+#Create group
+echo "Creating new group "$groupname" .."
+aws iam create-group --group-name $groupname > /dev/null
+
+#Attach policies to group
+echo "Attaching necessary policies to group.."
+aws iam attach-group-policy --group-name $groupname --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
+aws iam attach-group-policy --group-name $groupname --policy-arn arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy
+aws iam attach-group-policy --group-name $groupname --policy-arn arn:aws:iam::aws:policy/AmazonEKSServicePolicy
+aws iam attach-group-policy --group-name $groupname --policy-arn arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy
+aws iam attach-group-policy --group-name $groupname --policy-arn arn:aws:iam::aws:policy/AmazonEKSVPCResourceController
+
+#Add user to group
+echo "Adding user "$username" to group "$groupname" .."
+aws iam add-user-to-group --group-name $groupname --user-name $username
+
+echo $username" is ready. Please store the following access data securely:"
+echo "Access key ID: "$accessKeyId
+echo "Secret access key: "$secretAccessKey
+
