cde7156943ccd970ec1332d4e5d0c159fe2bc154
Joan Bellver <83341103+jbellver99@users.noreply.github.com>
2021-11-22 11:06:45 +0100

    Apply suggestions from code review

--- scripts/aws/sonarqubeinstance/datascript.sh
+++ scripts/aws/sonarqubeinstance/datascript.sh
@@ -12,4 +12,4 @@
                 sudo docker volume create sonarqube-logs
                 sudo docker volume create sonarqube-extensions
                 sudo docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 -v sonarqube-conf:/opt/sonarqube/conf -v sonarqube-data:/opt/sonarqube/data -v sonarqube-logs:/opt/sonarqube/logs -v sonarqube-extensions:/opt/sonarqube/extensions sonarqube:lts
-                echo "skript end"
+                echo "script end"
