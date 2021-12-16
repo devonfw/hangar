bdf06c42928e13109b9a011a9acc8a6cdc58dd88
Joan Bellver <83341103+jbellver99@users.noreply.github.com>
2021-12-16 08:38:08 +0100

    Update execute.sh

--- scripts/aws/sonarqubeinstance/execute.sh
+++ scripts/aws/sonarqubeinstance/execute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #The terraform init command will initialize the working directory containing Terraform configuration files and install any required plugins.
-terrafrom init
+terraform init
 #he terraform apply command performs a plan and actually carries out the planned changes to each resource using the relevant infrastructure provider's API.
 terraform apply --auto-approve
