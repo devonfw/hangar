c28a02a2e2afd9faca70c630eba895cbc201a55a
Yildiz <serdar.yildiz@capgemini.com>
2021-12-20 15:16:56 +0100

    change the location .tfvars, add the Documentation and delete execute.sh

--- scripts/aws/sonarqubeinstance/execute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#The terraform init command will initialize the working directory containing Terraform configuration files and install any required plugins.
-terraform init
-#he terraform apply command performs a plan and actually carries out the planned changes to each resource using the relevant infrastructure provider's API.
-terraform apply --auto-approve
