e2d7918ce6a3ced2cb87899007d5041ee64e8a1d
Sandesh, Pallapati Immanuel Prabhu <immanuel-prabhu.sandesh-pallapati@capgemini.com>
2021-12-28 13:05:53 +0530

    Update main.tf

--- scripts/accounts/aws/eks/main.tf
+++ scripts/accounts/aws/eks/main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "eu-west-1"
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 data "aws_eks_cluster" "cluster" {
@@ -13,8 +15,7 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.cluster.token 
 }
 
 data "aws_availability_zones" "available" {
