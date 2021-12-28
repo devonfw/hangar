3ebf43c0e0b1e162133d08c6f37500b1eb5c0f10
Sandesh, Pallapati Immanuel Prabhu <immanuel-prabhu.sandesh-pallapati@capgemini.com>
2021-12-28 14:40:28 +0530

    Update variables.tf

--- scripts/accounts/aws/eks/variables.tf
+++ scripts/accounts/aws/eks/variables.tf
@@ -1,33 +1,20 @@
-variable "aws_region" {
-  type = string
-  description = "location of the aws infrastructure deployment"
+ variable "region" {
+    description = "The region where to provision resources"
+    type = string
 }
-
+variable "access_key" {
+    description = "The access_key that belongs to the IAM user"
+    type = string
+}
+variable "secret_key" {
+    description = "The secret_key that belongs to the IAM user"
+    type = string
+}
+variable "cluster_name" {
+    description = "Cluster name for the AWS EKS"
+    type = string
+}
 variable "vpc_cidr_block" {
   type = string
   description = "define your vpc cidr block"
-}
-
-variable "subnet_cidr_block" {
-  type = string
-  description = "define your subnet cidr block"
-}
-variable "nic_private_ip" {
-    type = string
-    description = "the private ip needs to be within the subnet cidr range "
-}
-
-variable "instance_type" {
-  type = string
-  description = "instance_type"
-}
-
-variable "ami_ubuntu_20_04" {
-  type = string
-  description = "deploy Ubuntu 20.04 LTS"
-}
-
-variable "key_name" {
-  type = string
-  description = "Name of the key"
 }
