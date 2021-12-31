4071a4511d225c6f7b6602e503f3e0b8456e7c23
prathibhapadma <prathibha.padma@capgemini.com>
2021-12-31 09:13:52 +0530

    Update test_pipeline_generator.sh

--- scripts/pipelines/azure-devops/test_pipeline_generator.sh
+++ scripts/pipelines/azure-devops/test_pipeline_generator.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-## configuration file path to parse variables
-source ./pipeline_configuration.cfg
-
-while getopts n:l:d: flag
+while getopts n:l:d:c:b:w: flag
 do
     case "${flag}" in
         n) name=${OPTARG};;
         l) language=${OPTARG};;
         d) directory=${OPTARG};;
+        c) configuration_file=${OPTARG};;
+        b) targetbranch=${OPTARG};;
+        w) webBrowser=${OPTARG};;
                   
     esac
 done
@@ -22,7 +22,10 @@ then
     echo "  -n    [Required] Name that will be set to the test pipeline."
     echo "  -l    [Required] Language or framework of the project."
     echo "  -d    [Required] Local directory of your project (the path should always be using '/' and not '\')."
-       
+    echo "  -c    [Required] Configuration file Path containing all the variables."
+    echo "  -b               Name of the branch to which the Pull Request will target. PR is not created if the flag is not provided."
+    echo "  -w               Open the Pull Request on the web browser if it can not be automatically merged. Requires -b flag."
+    
     exit
 fi
 
@@ -48,21 +51,73 @@ hangarPath=$(pwd)
 echo -e "${green}Creating the new branch: feature/test-pipeline..."
 echo -e ${white}
 cd ${directory}
-git checkout -b ${branch[test_pipeline]}
+git checkout -b ${sourcebranch[test_pipeline]}
 cd ${hangarPath}
 
 # Move into the project's directory and pushing the template into the Azure DevOps repository.
 echo -e "${green}Committing and pushing to Git remote..."
 echo -e ${white}
 cd ${directory}
-git add $pipelines $scripts -f
+git add ${pipelines[test_pipeline]} ${scripts[test_pipeline]} -f
 git commit -m "Adding test pipeline source YAML"
-git push -u origin ${branch[test_pipeline]}
+git push -u origin ${sourcebranch[test_pipeline]}
 
 # Create Azure Pipeline
 echo -e "${green}Create Azure Test Pipeline:"
 echo -e ${white}
-az pipelines create --name $name --yml-path $pipelines 
+az pipelines create --name $name --yml-path ".pipelines/test-pipeline.yml" 
+
+# PR creation.
+if test -z "$targetbranch"
+then
+    # No branch specified in the parameters, no Pull Request is created, the code will be stored in the current branch.
+    echo -e "${green}No branch specified to do the Pull Request, changes left in the feature/test-pipeline branch."
+    echo -e $white
+    exit
+else
+    # Create the Pull Request to merge into the specified branch
+    echo -e "${green}Creating a Pull Request..."
+    echo -e ${white}
+    pr=$(az repos pr create --source-branch feature/test-pipeline --target-branch $targetbranch --title "Test new Pipeline" --auto-complete true)
+    # Obtain the PR id.
+    id=$(echo "$pr" | python -c "import sys, json; print(json.load(sys.stdin)['pullRequestId'])")
+    # Obtain the PR status.
+    showOutput=$(az repos pr show --id $id)
+    status=$(echo "$showOutput" | python -c "import sys, json; print(json.load(sys.stdin)['status'])")
+    # Check if the Pull Request merge has succeeded.
+    if test "$status" = "completed"
+    then
+        # Pull Request merged successfully.
+        echo -e "${green}Pull Request merged into $targetbranch branch successfully."
+        echo -e ${white}
+        exit
+    else
+        # Obtain the PR URL.
+        url=$(echo "$showOutput" | python -c "import sys, json; print(json.load(sys.stdin)['repository']['webUrl'])")
+        prURL="$url/pullrequest/$id"
+        # Check if the -w is activated.
+        flags=$*
+        if [[ "$flags" == *" -w"* ]]
+        then
+            # -w flag is activated and a page with the corresponding Pull Request is opened in the web browser.
+            echo -e "${green}Pull Request successfully created."
+            echo -e "${green}Opening the Pull Request on the web browser..."
+            echo -e ${white}
+            az repos pr show --id $id --open > /dev/null
+            exit
+        else
+            # -w flag is not activated and the URL to the Pull Request is shown in the console.
+            echo -e "${green}Pull Request successfully created."
+            echo -e "${green}To review the Pull Request and accept it, click on the following link:"
+            echo -e ${white}
+            echo ${prURL}
+            exit
+        fi
+    fi
+fi
+
+
+
 
 
 
