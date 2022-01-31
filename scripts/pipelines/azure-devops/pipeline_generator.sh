c39949d6103a6e1f94bbdf25c405d7f8055b9274
prathibhapadma <prathibha.padma@capgemini.com>
2022-01-31 18:05:44 +0530

    Update scripts/pipelines/azure-devops/pipeline_generator.sh
    Co-authored-by: albertdb <albertdb@users.noreply.github.com>

--- scripts/pipelines/azure-devops/pipeline_generator.sh
+++ scripts/pipelines/azure-devops/pipeline_generator.sh
@@ -19,7 +19,7 @@ then
     echo ""
     echo "Arguments:"
     echo "  -c    [Required] Configuration file containing parameters and variables."
-    echo "  -n    [Required] Name that will be set to the quality pipeline."
+    echo "  -n    [Required] Name that will be set to the pipeline."
     echo "  -d    [Required] Local directory of your project (the path should always be using '/' and not '\')."
     echo "  -l               Language or framework of the directory. Only required when generating Build and Test pipelines."
     echo "  -k               SonarQube project key. Only required when generating Quality pipelines."
