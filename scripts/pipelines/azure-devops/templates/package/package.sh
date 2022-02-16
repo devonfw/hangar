cb47df9fe7613ce557614e41eaa984a863c8e76a
TIMOTHE PATY <timothepaty@gmail.com>
2022-02-16 13:11:08 +0100

    deleting a white line

--- scripts/pipelines/azure-devops/templates/package/package.sh
+++ scripts/pipelines/azure-devops/templates/package/package.sh
@@ -13,10 +13,9 @@ do
 done
 
 branch_short=$(echo $branch | awk -F '/' '{ print $NF }')
-
 echo $branch | grep feature && imageName_completed="${imageName}_development" && tag_completed="${tag}_${branch_short}"
 echo $branch | grep develop && imageName_completed="${imageName}_test" && tag_completed="${tag}"
-(echo $branch | grep master || echo $branch | grep release ) && imageName_completed="${imageName}_test" && tag_completed="${tag}_${branch_short}"
+(echo $branch | grep master || echo $branch | grep release ) && imageName_completed="${imageName}_production" && tag_completed="${branch_short}"
 echo "docker build -f $dockerFile -t $imageName_completed:$tag_completed $context"
 docker build -f $dockerFile -t $imageName_completed:$tag_completed $context
 echo "docker login -u=$username -p=$password"
