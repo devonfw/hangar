8869912ffa020a2a512c35d25abe8e16f7adbf7e
jbellver99 <joan.bellver-fernandez@capgemini.com>
2022-02-25 12:42:21 +0100

    modified terrafom files

--- scripts/environment-provisioning/azure/aks/main.tf
+++ scripts/environment-provisioning/azure/aks/main.tf
@@ -17,6 +17,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     location = var.location
     resource_group_name = var.resource_group_name
     dns_prefix = var.dns_prefix
+    http_application_routing_enabled = true
 
     default_node_pool {
         name = "default"
