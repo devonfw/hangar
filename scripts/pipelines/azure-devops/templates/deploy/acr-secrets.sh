1fae38c9871a899986a761bd08951f8455670279
prathibhapadma <prathibha.padma@capgemini.com>
2022-03-17 17:52:24 +0530

    Update acr-secrets.sh

--- scripts/pipelines/azure-devops/templates/deploy/acr-secrets.sh
+++ scripts/pipelines/azure-devops/templates/deploy/acr-secrets.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #Command to create secrets to pull image from private registry.
-kubectl delete secret $(secretsValue) --namespace=$(k8sNamespace) --kubeconfig $(Pipeline.Workspace)/kubeconfig/kubeconfig
+kubectl delete secret $(secretsName) --namespace=$(k8sNamespace) --kubeconfig $(Pipeline.Workspace)/kubeconfig/kubeconfig
 # create secrets for private registry.
-kubectl create secret docker-registry $(secretsValue) \
+kubectl create secret docker-registry $(secretsName) \
     --docker-server=$(registry) \
     --docker-username=$(docker_username) \
     --docker-password=$(docker_password) \
     --namespace=$(k8sNamespace) \
     --kubeconfig $(Pipeline.Workspace)/kubeconfig/kubeconfig 
 # export secrets name and add secrets into deployment file.
-export secrets=$(secretsValue) 
+export secrets=$(secretsName) 
 yq e '.spec.template.spec."imagePullSecrets"=[{"name":"secrets"}]' -i $(Build.Repository.LocalPath)/$(deployFiles)/*-deployment.yaml 
 yq e '.spec.template.spec.imagePullSecrets[0].name = "'$secrets'"' -i $(Build.Repository.LocalPath)/$(deployFiles)/*-deployment.yaml 
 # Apply the changes.
