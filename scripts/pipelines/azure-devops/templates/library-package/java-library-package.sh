af39b4c7fe0f97b57eb11a8abdbd93c1cabd6307
Miguel A. Cerver <miguel-angel.cerver-copovi@capgemini.com>
2022-03-25 14:20:31 +0100

    envsubst usage and minor changes done

--- scripts/pipelines/azure-devops/templates/library-package/java-library-package.sh
+++ scripts/pipelines/azure-devops/templates/library-package/java-library-package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 settings_path="$(pwd)/.pipelines/scripts/settings.xml"
-mvn deploy --settings $settings_path
+mvn deploy -Dmaven.install.skip=true -Dmaven.test.skip=true --settings $settings_path
