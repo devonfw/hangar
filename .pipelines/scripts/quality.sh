765625450eef67a8f4bc8b0b3fc07ac94e7d6495
Zakariae Mkassi <97558392+ZakariaeMkassi@users.noreply.github.com>
2022-04-14 17:12:52 +0200

    Delete quality.sh

--- .pipelines/scripts/quality.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-url=http://1.2.3.4:9000
-login=6ce6663b63fc02881c6ea4c7cBa6563b8247a04e
-mvn sonar:sonar -Dsonar.host.url=$url -Dsonar.login=$login -Dsonar.java.binaries=$1/target/classes
