749c12ad95f85b457861b8b6a7ea717cdf981bf3
Sandesh, Pallapati Immanuel Prabhu <immanuel-prabhu.sandesh-pallapati@capgemini.com>
2022-04-19 10:26:28 +0530

    Modifications

--- scripts/pipelines/azure-devops/templates/eks/obtain-dns.sh
+++ scripts/pipelines/azure-devops/templates/eks/obtain-dns.sh
@@ -4,6 +4,4 @@ do
     sleep 5s
     dnsName=$(kubectl get svc --namespace nginx-ingress nginx-ingress-nginx-ingress-controller -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 done
-clustername=$1
-az pipelines variable-group create --organization $(System.TeamFoundationCollectionUri) --project "$(System.TeamProject)" --name eks_variables --variable dns_name=$dnsName cluster_name=$clustername --authorize true
 echo "##vso[task.setvariable variable=dns;isOutput=true]$dnsName"
