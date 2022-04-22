7a700d28f140c42bd4b870a0cceb37b9b7c5b8df
jbellver99 <joan.bellver-fernandez@capgemini.com>
2022-04-22 09:44:59 +0200

    Address sonatypee-lift suggestions

--- scripts/pipelines/azure-devops/templates/aks/obtain-dns.sh
+++ scripts/pipelines/azure-devops/templates/aks/obtain-dns.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ip=$(kubectl get svc nginx-ingress-nginx-ingress-controller --namespace nginx-ingress --kubeconfig $1/kubeconfig -o jsonpath='{.status.loadBalancer.ingress[0].ip}' &> /dev/null)
+ip="$(kubectl get svc nginx-ingress-nginx-ingress-controller --namespace nginx-ingress --kubeconfig $1/kubeconfig -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
 
 while test -z "$ip"
 do
     sleep 5s
-    ip=$(kubectl get svc nginx-ingress-nginx-ingress-controller --namespace nginx-ingress --kubeconfig $1/kubeconfig -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    ip="$(kubectl get svc nginx-ingress-nginx-ingress-controller --namespace nginx-ingress --kubeconfig $1/kubeconfig -o jsonpath='{.status.loadBalancer.ingress[0].ip}')"
 done
 
 url=$2
@@ -16,8 +16,8 @@ ipname=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddr
 
 iprg=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$ip')].[resourceGroup]" --output tsv)
 
-az network public-ip update --resource-group $iprg --name $ipname --dns-name $dnsname
+az network public-ip update --resource-group "$iprg" --name "$ipname" --dns-name "$dnsname"
 
-dns=$(az network public-ip show --resource-group $iprg --name $ipname --query "[dnsSettings.fqdn]" --output tsv)
+dns="$(az network public-ip show --resource-group $iprg --name $ipname --query "[dnsSettings.fqdn]" --output tsv)"
 
 echo "##vso[task.setvariable variable=dns;]$dns"
