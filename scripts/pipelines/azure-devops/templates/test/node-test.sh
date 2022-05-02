df74f8297c9b49efb8a3c8fe70983ce53fd97ee9
zchriste <zina.christensen@capgemini.com>
2022-05-02 11:50:48 +0200

    added jest-junit config options as env variables to test pipeline script

--- scripts/pipelines/azure-devops/templates/test/node-test.sh
+++ scripts/pipelines/azure-devops/templates/test/node-test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npm install
-npm run test -- --ci --reporters=default --reporters=jest-junit
+npm install jest
+npm install jest-junit
+JEST_SUITE_NAME="jest tests" JEST_JUNIT_OUTPUT_NAME="TEST-junit.xml" JEST_JUNIT_OUTPUT_DIR="./test-data" npm run test -- --ci --reporters=default --reporters=jest-junit
