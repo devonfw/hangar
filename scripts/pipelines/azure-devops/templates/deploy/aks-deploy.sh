ffd7efe20e29f475bc8d4c6c504854efac9af602
prathibhapadma <prathibha.padma@capgemini.com>
2022-05-06 15:22:10 +0530

    updates

--- scripts/pipelines/azure-devops/templates/deploy/aks-deploy.sh
+++ scripts/pipelines/azure-devops/templates/deploy/aks-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Add image name and ingress DNS name.
-export image=$1 dns=$2
-yq eval '.spec.template.spec.containers[0].image = "'$image'"' -i $3 
-yq eval '.spec.rules[0].host = "'$dns'"' -i $4
+export image="$1" dns="$2"
+yq eval '.spec.template.spec.containers[0].image = "'$image'"' -i "$3" 
+yq eval '.spec.rules[0].host = "'$dns'"' -i "$4"
 # Create namespace in AKS cluster.
-kubectl create namespace $6 --kubeconfig $7
+kubectl create namespace "$6" --kubeconfig "$7"
 # Apply manifest files.
-kubectl apply -f $5 --namespace=$6 --kubeconfig $7
+kubectl apply -f "$5" --namespace="$6" --kubeconfig "$7"
--- scripts/pipelines/azure-devops/templates/deploy/eks-deploy.sh
+++ scripts/pipelines/azure-devops/templates/deploy/eks-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Add image name and ingress DNS name.
-export image=$2 dns=$3
-yq eval '.spec.template.spec.containers[0].image = "'$image'"' -i $4
-yq eval '.spec.rules[0].host = "'$dns'"' -i $5
+export image="$2" dns="$3"
+yq eval '.spec.template.spec.containers[0].image = "'$image'"' -i "$4"
+yq eval '.spec.rules[0].host = "'$dns'"' -i "$5"
 # Create namespace in EKS cluster.
-kubectl create namespace $1
+kubectl create namespace "$1"
 # Apply manifest files.
-kubectl apply -f $6 --namespace=$1
+kubectl apply -f "$6" --namespace="$1"
