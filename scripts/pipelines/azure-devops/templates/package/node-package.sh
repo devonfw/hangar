111931c0ad50197a05b2880f6cee260576178c69
zchriste <zina.christensen@capgemini.com>
2022-05-09 09:54:54 +0200

    refactored package pipeline to have generic and language specific part

--- scripts/pipelines/azure-devops/templates/package/node-package.sh
+++ scripts/pipelines/azure-devops/templates/package/node-package.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 ################################ Description ############################################
-# This script is used to build and push a quarkus project image to a container registry
+# This script is used to build and push a node project image to a container registry
 ################################# Arguments #############################################
-# -t : Path to the pom.xml
+# -t : Path to the package.json
 #########################################################################################
 set -e
 
 while getopts t: flag
 do
     case "${flag}" in
-        t) pomPath=${OPTARG};;
+        t) packagePath=${OPTARG};;
     esac
 done
-# We define the tag using the version set in the pom.xml
-tag=$(grep version "${pomPath}" | grep -v -e '<?xml'| head -n 1 | sed 's/[[:space:]]//g' | sed -E 's/<.{0,1}version>//g' | awk '{print $1}')
+
+# We define the tag using the version set in the package.json
+tag=$(grep version "${packagePath}" | sed 's/.*"version": "\(.*\)".*/\1/')
 
 SCRIPT_PATH="./generic-package.sh"
 # shellcheck source="./generic-package.sh"
