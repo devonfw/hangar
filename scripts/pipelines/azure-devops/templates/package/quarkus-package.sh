6a399302005e94d17961f36775aeebeb88de534b
Patrick O. Baidoo-Antwi <poba2010@live.com>
2022-05-17 15:52:31 +0200

    double quote shell variables in quarkus-package.sh

--- scripts/pipelines/azure-devops/templates/package/quarkus-package.sh
+++ scripts/pipelines/azure-devops/templates/package/quarkus-package.sh
@@ -32,23 +32,23 @@ do
     esac
 done
 # We define the tag using the version set in the pom.xml
-tag=$(grep version ${pomPath} | grep -v -e '<?xml'| head -n 1 | sed 's/[[:space:]]//g' | sed -E 's/<.{0,1}version>//g' | awk '{print $1}')
+tag=$(grep version "${pomPath}" | grep -v -e '<?xml'| head -n 1 | sed 's/[[:space:]]//g' | sed -E 's/<.{0,1}version>//g' | awk '{print $1}')
 # we get what is located after the last '/' in the branch name, so it removes /ref/head or /ref/head/<folder> if your branche is named correctly"
-branch_short=$(echo $branch | awk -F '/' '{ print $NF }')
+branch_short=$(echo "$branch" | awk -F '/' '{ print $NF }')
 
 # We change the name of the tag depending if it is a release or another branch
-echo $branch | grep release && tag_completed="${tag}"
-echo $branch | grep release || tag_completed="${tag}_${branch_short}"
+echo "$branch" | grep release && tag_completed="${tag}"
+echo "$branch" | grep release || tag_completed="${tag}_${branch_short}"
 
 # We build the image
 echo "docker build -f $dockerFile -t $imageName:$tag_completed $context"
-docker build -f $dockerFile -t $imageName:$tag_completed $context
+docker build -f "$dockerFile" -t "$imageName:$tag_completed" "$context"
 
 # We connect to the registry
 if test -z "$aws_access_key"
 then
     echo "docker login -u=**** -p=**** $registry"
-    docker login -u="$username" -p="$password" $registry
+    docker login -u="$username" -p="$password" "$registry"
 else
     aws configure set aws_access_key_id "$aws_access_key"
     aws configure set aws_secret_access_key "$aws_secret_access_key"
@@ -58,13 +58,13 @@ fi
 
 # We push the image previously built
 echo "docker push $imageName:$tag_completed"
-docker push $imageName:$tag_completed
+docker push "$imageName:$tag_completed"
 
 # If this is a release we push it a second time with "latest" tag
-if echo $branch | grep release
+if echo "$branch" | grep release
 then
     echo "Also pushing the image as 'latest' if this is a release"
     docker tag "$imageName:$tag_completed" "$imageName:latest"
     echo "docker push $imageName:latest"
-    docker push $imageName:latest
+    docker push" $imageName":latest
 fi
