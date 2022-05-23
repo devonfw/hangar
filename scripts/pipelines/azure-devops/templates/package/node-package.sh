cdc26f04fb8911ce3ab8b1e59d13932e1d26e595
zchriste <zina.christensen@capgemini.com>
2022-05-23 10:33:04 +0200

    added usable flags to language specific scripts, removed -t tag + arg from package-common.sh receiving arguments, changed script path

--- scripts/pipelines/azure-devops/templates/package/node-package.sh
+++ scripts/pipelines/azure-devops/templates/package/node-package.sh
@@ -6,9 +6,10 @@
 #########################################################################################
 set -e
 
-while getopts t: flag
+while getopts f:c:u:p:r:i:b:a:s:l:t: flag
 do
     case "${flag}" in
+        f) ;; c) ;; u) ;; p) ;; r) ;; i) ;; b) ;; a) ;; s) ;; l) ;;
         t) packagePath=${OPTARG};;
         *) echo "flag ${flag} not specified"
     esac
@@ -17,6 +18,17 @@ done
 # We define the tag using the version set in the package.json
 tag=$(grep version "${packagePath}" | sed 's/.*"version": "\(.*\)".*/\1/')
 
-SCRIPT_PATH="./templates/package/package-common.sh"
+# filter -t tag from arguments
+args=("${@}")
+filter="-t"
+
+for ((i=0; i<"${#args[@]}"; ++i));
+do
+    case ${args[i]} in
+        "$filter") unset "args[i]"; unset "args[i+1]"; break ;;
+    esac
+done
+
+SCRIPT_PATH="./.pipelines/scripts/package-common.sh"
 # shellcheck source="package-common.sh"
-source "$SCRIPT_PATH" "$@" -q "$tag"
+source "$SCRIPT_PATH" "${args[@]}" -q "$tag"
--- scripts/pipelines/azure-devops/templates/package/quarkus-package.sh
+++ scripts/pipelines/azure-devops/templates/package/quarkus-package.sh
@@ -6,9 +6,10 @@
 #########################################################################################
 set -e
 
-while getopts t: flag
+while getopts f:c:u:p:r:i:b:a:s:l:t: flag
 do
     case "${flag}" in
+        f) ;; c) ;; u) ;; p) ;; r) ;; i) ;; b) ;; a) ;; s) ;; l) ;;
         t) pomPath=${OPTARG};;
         *) echo "flag ${flag} not specified"
     esac
@@ -16,6 +17,17 @@ done
 # We define the tag using the version set in the pom.xml
 tag=$(grep version "${pomPath}" | grep -v -e '<?xml'| head -n 1 | sed 's/[[:space:]]//g' | sed -E 's/<.{0,1}version>//g' | awk '{print $1}')
 
-SCRIPT_PATH="./templates/package/package-common.sh"
+# filter -t tag from arguments
+args=("${@}")
+filter="-t"
+
+for ((i=0; i<"${#args[@]}"; ++i));
+do
+    case ${args[i]} in
+        "$filter") unset "args[i]"; unset "args[i+1]"; break ;;
+    esac
+done
+
+SCRIPT_PATH="./.pipelines/scripts/package-common.sh"
 # shellcheck source="package-common.sh"
-source "$SCRIPT_PATH" "$@" -q "$tag"
+source "$SCRIPT_PATH" "${args[@]}" -q "$tag"
