0b32f4214d76b76382e9cc6b00cd066016b4b663
Patrick O. Baidoo-Antwi <poba2010@live.com>
2022-06-01 12:13:14 +0200

    wip fix build script

--- scripts/pipelines/azure-devops/templates/build/dotnet-build.sh
+++ scripts/pipelines/azure-devops/templates/build/dotnet-build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-dotnet build -c Release -o "$targetDirectory"
+echo "debug:building .net"
+# dotnet build "Templates/WebAPI/Devon4Net.Application.WebAPI/Devon4Net.Application.WebAPI.csproj" -c Release
+dotnet build -c Release
+
+echo "debug:publishing ... ${targetDirectory}"
+# dotnet publish "Templates/WebAPI/Devon4Net.Application.WebAPI/Devon4Net.Application.WebAPI.csproj" -c Release -o "${targetDirectory}"
+dotnet publish -c Release -o "${targetDirectory}"
+
