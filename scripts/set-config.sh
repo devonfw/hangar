519aa096b1f7a6aa685e97ff2c938a22ece5b851
ultymatom <90399827+ultymatom@users.noreply.github.com>
2022-06-09 13:00:09 +0200

    indentation fix

--- scripts/set-config.sh
+++ scripts/set-config.sh
@@ -9,29 +9,29 @@ for arg in "${@}"
 do
     if [[ "$arg" == "-"* ]]; then
         if [[ "$pingpong" == "ping" ]]; then
-           echo -e "${red}ERROR: Missing value for variable $key"
-           exit 1
+            echo -e "${red}ERROR: Missing value for variable $key"
+            exit 1
         fi
         pingpong="ping"
-		key="${arg//-/}"
-		if echo "$key" | grep "=" > /dev/null
-		then
-			key_tmp="$key"
-			key=$(echo "$key_tmp" | cut -d'=' -f1)
-			value=$(echo "$key_tmp" | cut -d'=' -f2)
-			pingpong="pong"
-		fi
+	key="${arg//-/}"
+	if echo "$key" | grep "=" > /dev/null
+	then
+	    key_tmp="$key"
+	    key=$(echo "$key_tmp" | cut -d'=' -f1)
+	    value=$(echo "$key_tmp" | cut -d'=' -f2)
+	    pingpong="pong"
+	fi
     else
         if [[ "$pingpong" == "pong" ]]; then
-           echo -e "${red}ERROR: Received two values for variable $key or variable not properly passed as flag"
-           exit 1
+            echo -e "${red}ERROR: Received two values for variable $key or variable not properly passed as flag"
+            exit 1
         fi
         pingpong="pong"
         value="$arg"
-	fi
+    fi
     if [[ "$pingpong" == "pong" ]]
-	then
-		if grep -qe "$key *= *" $tfvars
+    then
+	if grep -qe "$key *= *" $tfvars
         then
             sed -i "s|\($key *= *\).*|\1\"$value\"|" "$tfvars"
         else
