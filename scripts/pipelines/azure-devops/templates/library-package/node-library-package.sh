2f0019fb838c3e96ee48b956f305202c88842917
shiftwolf <54272779+shiftwolf@users.noreply.github.com>
2022-06-09 08:38:03 +0200

    refactor / bugfix

--- scripts/pipelines/azure-devops/templates/library-package/node-library-package.sh
+++ scripts/pipelines/azure-devops/templates/library-package/node-library-package.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-npmrc_path="$(pwd)/.pipelines/scripts/.npmrc"
-npm publish --userconfig "$npmrc_path"
+npmrc_path="$(pwd)/.pipelines/scripts"
+cp "${npmrc_path}/.npmrc.project.template" "$(pwd)/.npmrc"
+npm publish --userconfig "${npmrc_path}/.npmrc"
