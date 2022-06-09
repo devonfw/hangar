126a950bf7a18f0ab6f3ab0a46550b6d971faeea
ultymatom <90399827+ultymatom@users.noreply.github.com>
2022-06-09 12:54:40 +0200

    Update set-config.sh
    Fixing sonatype and handling "=" and spaces in value

--- scripts/set-config.sh
+++ scripts/set-config.sh
@@ -3,10 +3,9 @@
 tfvars="./terraform.tfvars"
 
 red='\e[0;31m'
-    
-list_of_args=( "${@//=/ }" )
+
 pingpong="pong"
-for arg in ${list_of_args[@]}
+for arg in "${@}"
 do
     if [[ "$arg" == "-"* ]]; then
         if [[ "$pingpong" == "ping" ]]; then
@@ -14,15 +13,25 @@ do
            exit 1
         fi
         pingpong="ping"
-        key=${arg//-/}
+		key="${arg//-/}"
+		if echo "$key" | grep "=" > /dev/null
+		then
+			key_tmp="$key"
+			key=$(echo "$key_tmp" | cut -d'=' -f1)
+			value=$(echo "$key_tmp" | cut -d'=' -f2)
+			pingpong="pong"
+		fi
     else
         if [[ "$pingpong" == "pong" ]]; then
            echo -e "${red}ERROR: Received two values for variable $key or variable not properly passed as flag"
            exit 1
         fi
         pingpong="pong"
-        value=$arg
-        if grep -qe "$key *= *" $tfvars
+        value="$arg"
+	fi
+    if [[ "$pingpong" == "pong" ]]
+	then
+		if grep -qe "$key *= *" $tfvars
         then
             sed -i "s|\($key *= *\).*|\1\"$value\"|" "$tfvars"
         else
