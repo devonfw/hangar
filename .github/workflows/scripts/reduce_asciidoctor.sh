a600173371685c6acc7289037ef6ec81bcac0103
Timothe <timothepaty@gmail.com>
2022-06-16 16:59:45 +0200

    test remove a path froñ find

--- .github/workflows/scripts/reduce_asciidoctor.sh
+++ .github/workflows/scripts/reduce_asciidoctor.sh
@@ -1,4 +1,4 @@
-for i in $(find documentation/src -type f -name "*asciidoc")
+for i in $(find documentation/src -type f -name "*asciidoc" -not -path "*common_templates*")
 do
   asciidoctor-reducer -o "${i//src\//}" "$i"
 done
