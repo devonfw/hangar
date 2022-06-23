5b57ad1b49833ffb713d2e1633c7d6e10d091a34
shiftwolf <54272779+shiftwolf@users.noreply.github.com>
2022-06-23 21:54:51 +0200

    changes

--- scripts/pipelines/azure-devops/templates/build/generic-build.sh
+++ scripts/pipelines/azure-devops/templates/build/generic-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # We take the first 2 elements of the array because the vso command has a space and is therefore split up by bash.
-vsoCommand=$(task build | { read -a array ; echo ${array[@]:0:2} ; })
-
+vsoCommand=$(task build-configuration | { read -a array ; echo ${array[@]:0:2} ; })
 echo $vsoCommand
+
+task build
