dab6aff8d72c278f86270f63f87dccff927ab141
shiftwolf <54272779+shiftwolf@users.noreply.github.com>
2022-06-23 22:19:11 +0200

    first draft

--- scripts/pipelines/azure-devops/templates/build/generic-build.sh
+++ scripts/pipelines/azure-devops/templates/build/generic-build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
+
+cd "./.pipelines/scripts"
+
 # We take the first 2 elements of the array because the vso command has a space and is therefore split up by bash.
-vsoCommand=$(task build-configuration | { read -a array ; echo ${array[@]:0:2} ; })
+vsoCommand=$(../../bin/task build-configuration | { read -a array ; echo ${array[@]:0:2} ; })
 echo $vsoCommand
 
-task build
+../../bin/task build
