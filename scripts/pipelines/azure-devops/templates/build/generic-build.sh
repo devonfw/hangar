a2062a5a5896fa1681ea7b5481bb4a954e54dd6b
shiftwolf <54272779+shiftwolf@users.noreply.github.com>
2022-06-24 10:26:14 +0200

    Update generic-build.sh

--- scripts/pipelines/azure-devops/templates/build/generic-build.sh
+++ scripts/pipelines/azure-devops/templates/build/generic-build.sh
@@ -3,7 +3,7 @@
 # Install taskfile
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
 
-cd "./.pipelines/scripts" || { echo -e "${red}Error: ./.pipelines/scripts could not be found." >&2; exit 1; }
+cd "./.pipelines/scripts" || { echo -e "Error: ./.pipelines/scripts could not be found." >&2; exit 1; }
 
 # We take the first 2 elements of the array because the vso command has a space and is therefore split up by bash.
 vsoCommand=$(../../bin/task build-configuration | { read -r -a array ; echo "${array[@]:0:2}" ; })
