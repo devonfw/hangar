9775fd42ed17c367633e22c38c841815987a2b2b
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-18 09:35:51 +0530

    updated with minor changes in deploy scripts

--- scripts/pipelines/azure-devops/templates/deploy/deploy.sh
+++ scripts/pipelines/azure-devops/templates/deploy/deploy.sh
@@ -4,7 +4,6 @@ set -e
 export image="$2" dns="$3"
 yq eval '.spec.template.spec.containers[0].image = "'"$image"'"' -i "$4"
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
-# Create namespace in EKS cluster.
-kubectl create namespace "$1"
-# Apply manifest files.
+# Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
+kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
 kubectl apply -f "$6" --namespace="$1"
--- scripts/pipelines/azure-devops/templates/deploy/secrets.sh
+++ scripts/pipelines/azure-devops/templates/deploy/secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-#Command to create secrets to pull image from private registry.
-kubectl delete secret "$2" --namespace="$1" 
-# create secrets for private registry.
-kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1" 
-# export secrets name and add secrets into deployment file.
+# Deploy apps in exists namespace, If not exists Create new namespace.
+kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
+# Command to existed secrets and create new secrets to pull image from private registry.
+kubectl delete secret "$2" --namespace="$1" --ignore-not-found 
+kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1"
 export secrets="$2" 
 yq e '.spec.template.spec."imagePullSecrets"=[{"name":"secrets"}]' -i "$6" 
 yq e '.spec.template.spec.imagePullSecrets[0].name = "'"$secrets"'"' -i "$6"
