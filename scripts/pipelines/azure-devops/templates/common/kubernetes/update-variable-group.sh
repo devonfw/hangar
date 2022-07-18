4e1e35222c51c1a0088fb1b8aa5832c32d7dfd1e
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-18 19:27:05 +0530

    updated with minor changes and src folder documents

--- scripts/pipelines/azure-devops/templates/common/kubernetes/update-variable-group.sh
+++ scripts/pipelines/azure-devops/templates/common/kubernetes/update-variable-group.sh
@@ -5,4 +5,4 @@ az pipelines variable-group list --organization "$1" --project "$2" --group-name
 variablegroup=$(az pipelines variable-group list --organization "$1" --project "$2" --group-name "$3" --query '[0].name' -o  json)
 groupid=$(az pipelines variable-group list --organization "$1" --project "$2" --group-name "$3" --query '[0].id' -o  json)
 # Update variables, If already exists variable group.
-az pipelines variable-group variable list --organization "$1" --project "$2" --group-id "$groupid" | grep -q "^$4" || az pipelines variable-group variable update --organization "$1" --project "$2" --group-id $groupid --name "$4" --value "$5"
+az pipelines variable-group variable list --organization "$1" --project "$2" --group-id "$groupid" | grep -q "^$4" || az pipelines variable-group variable create --organization "$1" --project "$2" --group-id $groupid --name "$4" --value "$5"
