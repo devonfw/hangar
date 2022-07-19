ee16702e6e88dc592f022117ac0250a5848d0b75
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-19 09:09:59 +0530

    update with tag

--- scripts/pipelines/github/templates/deploy/deploy.sh
+++ scripts/pipelines/github/templates/deploy/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
-# Add image name and ingress DNS name.
-export image="$2" dns="$3"
-yq eval '.spec.template.spec.containers[0].image = "'"$image"'"' -i "$4"
+# Add image name, dns_name and tag.
+source "$7"
+export image="$2" tag="${tag}" dns="$3"
+yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag"'"' -i "$4"
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
 # Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
