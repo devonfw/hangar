751a0c76950cd6ae7fa08486ae9271a345a60806
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-20 08:12:48 +0530

    updated with minor change

--- scripts/pipelines/azure-devops/templates/common/kubernetes/update-variable-group.sh
+++ scripts/pipelines/azure-devops/templates/common/kubernetes/update-variable-group.sh
@@ -3,5 +3,5 @@ set -e
 # Create new variable group.
 az pipelines variable-group list --organization "$1" --project "$2" --group-name "$3" --query '[0].name' -o  table | grep -q "^$3" || az pipelines variable-group create --organization "$1" --project "$2" --name "$3" --variables "$4"="$5"
 groupid=$(az pipelines variable-group list --organization "$1" --project "$2" --group-name "$3" --query '[0].id' -o  json)
-# Update variables, If already exists variable group.
-az pipelines variable-group variable list --organization "$1" --project "$2" --group-id "$groupid" | grep -q "^$4" || az pipelines variable-group variable create --organization "$1" --project "$2" --group-id "$groupid" --name "$4" --value "$5"
+# Update variable value, If already exists or Create new variable.
+az pipelines variable-group variable update --organization "$1" --project "$2" --group-id $groupid --name "$4" --value "$5" || az pipelines variable-group variable create --organization "$1" --project "$2" --group-id $groupid --name "$4" --value "$5"
