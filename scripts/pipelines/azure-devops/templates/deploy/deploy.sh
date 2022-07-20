5694b38caa6d8f0907b4ac1772953b3af7ec5443
albertdb <albertdb@users.noreply.github.com>
2022-07-20 14:16:07 +0200

    Update deploy.sh

--- scripts/pipelines/azure-devops/templates/deploy/deploy.sh
+++ scripts/pipelines/azure-devops/templates/deploy/deploy.sh
@@ -9,3 +9,4 @@ yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
 # Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
 kubectl apply -f "$6" --namespace="$1"
+kubectl rollout restart deploy --namespace="$1"
