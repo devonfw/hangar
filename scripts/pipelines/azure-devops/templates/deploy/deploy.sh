06822bfea2ca9ccdc62f0c9e5940c8685d9bf889
albertdb <albertdb@users.noreply.github.com>
2022-07-20 15:08:22 +0200

    Apply suggestions from code review

--- scripts/pipelines/azure-devops/templates/deploy/deploy.sh
+++ scripts/pipelines/azure-devops/templates/deploy/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 # Add image name, dns_name and tag.
-# Run package-extra.sh to ${tag}
+# Source package-extra.sh to get ${tag}
 . "$7"
 export image="$2" tag="${tag}" dns="$3"
 yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag"'"' -i "$4"
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
-# Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
+# Create namespace if not exists 
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
+# Apply manifest files
 kubectl apply -f "$6" --namespace="$1"
+# Rollout deployments in the namespace
 kubectl rollout restart deploy --namespace="$1"
--- scripts/pipelines/azure-devops/templates/deploy/secrets.sh
+++ scripts/pipelines/azure-devops/templates/deploy/secrets.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
-# Deploy apps in exists namespace, If not exists Create new namespace.
+# Create namespace if not exists 
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
-# Command to create secrets to pull image from private registry.
+# Fill imagePullSecrets
 export secrets="$2" 
 yq e '.spec.template.spec."imagePullSecrets"=[{"name":"secrets"}]' -i "$6" 
 yq e '.spec.template.spec.imagePullSecrets[0].name = "'"$secrets"'"' -i "$6"
 # Apply the changes.
+# Create imagePullSecret if not exists
 kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1" --save-config --dry-run=client -o yaml | kubectl apply -f - --namespace="$1"
