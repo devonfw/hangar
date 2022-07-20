aa081e13a1160b668465fe826e27f3c5c1113874
albertdb <albertdb@users.noreply.github.com>
2022-07-20 14:50:17 +0200

    Update update-variable-group.sh

--- scripts/pipelines/azure-devops/templates/common/kubernetes/update-variable-group.sh
+++ scripts/pipelines/azure-devops/templates/common/kubernetes/update-variable-group.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-groupid=$(az pipelines variable-group list --group-name "$1" --query '[0].id' -o  json)
-if test -z $groupid
-then
-    az pipelines variable-group create --name "$1" --variables "$2"="$3"
-else 
-    az pipelines variable-group variable create --group-id "$groupid" --name "$2" --value "$3"
-fi
+set -e
+# Create new variable group.
+az pipelines variable-group list --organization "$1" --project "$2" --group-name "$3" --query '[0].name' -o  table | grep -q "^$3" || az pipelines variable-group create --organization "$1" --project "$2" --name "$3" --variables "$4"="$5"
+groupid=$(az pipelines variable-group list --organization "$1" --project "$2" --group-name "$3" --query '[0].id' -o  json)
+# Update variable value, If already exists or Create new variable.
+az pipelines variable-group variable update --organization "$1" --project "$2" --group-id $groupid --name "$4" --value "$5" || az pipelines variable-group variable create --organization "$1" --project "$2" --group-id $groupid --name "$4" --value "$5"
