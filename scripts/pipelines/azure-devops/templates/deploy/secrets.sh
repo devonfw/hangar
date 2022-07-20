5ec71228c2f960589e30da97e9c46f7bbd77afd4
prathibhapadma <prathibha.padma@capgemini.com>
2022-07-20 17:24:44 +0530

    Update scripts/pipelines/azure-devops/templates/deploy/secrets.sh
    Co-authored-by: albertdb <albertdb@users.noreply.github.com>

--- scripts/pipelines/azure-devops/templates/deploy/secrets.sh
+++ scripts/pipelines/azure-devops/templates/deploy/secrets.sh
@@ -7,4 +7,4 @@ export secrets="$2"
 yq e '.spec.template.spec."imagePullSecrets"=[{"name":"secrets"}]' -i "$6" 
 yq e '.spec.template.spec.imagePullSecrets[0].name = "'"$secrets"'"' -i "$6"
 # Apply the changes.
-kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1" --save-config --dry-run=none -o yaml | kubectl apply -f "$6" --namespace="$1"
+kubectl create secret docker-registry "$2" --docker-server="$5" --docker-username="$3" --docker-password="$4" --namespace="$1" --save-config --dry-run=client -o yaml | kubectl apply -f - --namespace="$1"
