075cfb38f44211b0c3c380d9aeaf838a20da0f05
ultymatom <90399827+ultymatom@users.noreply.github.com>
2022-07-21 08:49:37 +0200

    tag_completed was unused

--- scripts/pipelines/github/templates/deploy/deploy.sh
+++ scripts/pipelines/github/templates/deploy/deploy.sh
@@ -11,7 +11,7 @@ echo "tag_completed: $8" | grep release && tag_completed="${tag}"
 echo "tag_completed_branch: $8" | grep release || tag_completed="${tag}_${branch_short}"
 
 export image="$2" tag="${tag}" dns="$3"
-yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag"'"' -i "$4"
+yq eval '.spec.template.spec.containers[0].image = "'"$image:$tag_completed"'"' -i "$4"
 yq eval '.spec.rules[0].host = "'"$dns"'"' -i "$5"
 # Deploy apps in exists namespace, If not exists Create new namespace and apply manifest files.
 kubectl get namespace | grep -q "^$1" || kubectl create namespace "$1"
