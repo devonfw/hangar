f7233969d5fd4e066122f416b8722b6f98077c52
Rodenas López <jonathan.rodenas-lopez@capgemini.com>
2022-10-06 12:42:49 +0200

    Check for CRLF newline

---
+++ .github/workflows/scripts/line-ending-check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+output=$(find . -not -type d -print0 | xargs -0 dos2unix -ic | cut -c 3-)
+if [ "$output" == "" ] ; then
+    exit 0
+else
+    echo "The following files have Windows line ending:"
+    echo ""
+    echo "$output"
+    echo ""
+    echo "No changes were made on the repository. Please change line endings to Unix style for passing this check."
+    
+    exit 1
+fi
