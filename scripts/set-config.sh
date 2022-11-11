8697679e1b0af8c5bd55131fe27fadde3525e479
Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2022-11-11 08:52:19 +0100

    add some quotes and split the cahnge color white to a new line

--- scripts/set-config.sh
+++ scripts/set-config.sh
@@ -12,7 +12,8 @@ do
     ## Read Key
     if [[ "$arg" == "--"* ]]; then
         if [[ "$pingpong" == "ping" ]]; then
-            echo -e "${red}ERROR: Missing value for variable $key ${white}"
+            echo -e "${red}ERROR: Missing value for variable $key."
+            echo -e "${white}"
             exit 1
         fi
         pingpong="ping"
@@ -28,7 +29,8 @@ do
     # Read Value
     else
         if [[ "$pingpong" == "pong" ]]; then
-            echo -e "${red}ERROR: Received two values for variable $key or variable not properly passed as flag ${white}"
+            echo -e "${red}ERROR: Received two values for variable $key or variable not properly passed as flag."
+            echo -e "${white}"
             exit 1
         fi
         pingpong="pong"
@@ -48,7 +50,7 @@ do
             if [[ -n "$file_set_vars" ]]
             then
                 # Check if key is already in file to replace it
-                if grep -qe "$key *= *" $file_set_vars
+                if grep -qe "$key *= *" "$file_set_vars"
                 then
                     sed -i "s|\($key *= *\).*|\1\"$value\"|" "$file_set_vars"
                 # Write new line with the new key=value
@@ -56,7 +58,8 @@ do
                     echo "$key=\"$value\"" >> "$file_set_vars"
                 fi
             else
-                echo -e "${red}ERROR: Missing file where vars are stored. It must be the first arg (key and value) and the key to set it is: --file_set_vars ${white}"
+                echo -e "${red}ERROR: Missing file where vars are stored. It must be the first arg (key and value) and the key to set it is: --file_set_vars."
+                echo -e "${white}"
                 exit 1
             fi
         fi
--- scripts/sonarqube/common/setup_sonarqube.sh
+++ scripts/sonarqube/common/setup_sonarqube.sh
@@ -4,11 +4,11 @@ white='\e[1;37m'
 green='\e[1;32m'
 
 echo -e "${green}Updating repositories..."
-echo -e ${white}
+echo -e "${white}"
 sudo apt-get update
 
 echo -e "${green}Setting proper system limits for SonarQube..."
-echo -e ${white}
+echo -e "${white}"
 sudo bash -c 'cat > /etc/sysctl.d/99-sonarqube.conf' << EOF 
 vm.max_map_count=524288
 fs.file-max=131072
@@ -17,15 +17,15 @@ sudo sysctl -w vm.max_map_count=524288
 sudo sysctl -w fs.file-max=131072
 
 echo -e "${green}Installing Docker..."
-echo -e ${white}
+echo -e "${white}"
 sudo apt-get install docker.io -y
 
 echo -e "${green}Pulling the SonarQube LTS image..."
-echo -e ${white}
+echo -e "${white}"
 sudo docker pull sonarqube:lts
 
 echo -e "${green}Creating the default volumes for SonarQube..."
-echo -e ${white}
+echo -e "${white}"
 sudo docker volume create sonarqube-conf 
 sudo docker volume create sonarqube-data
 sudo docker volume create sonarqube-logs
@@ -40,6 +40,6 @@ mem=$(echo "scale=2;$mem*0.85" |bc)
 mem="$mem"m
 
 echo -e "${green}Launching SonarQube container..."
-echo -e ${white}
+echo -e "${white}"
 sudo docker run -d --restart always --memory "$mem" --name sonarqube -p 9000:9000 -p 9092:9092 --ulimit nofile=131072 --ulimit nproc=8192 -v sonarqube-conf:/opt/sonarqube/conf -v sonarqube-data:/opt/sonarqube/data -v sonarqube-logs:/opt/sonarqube/logs -v sonarqube-extensions:/opt/sonarqube/extensions sonarqube:lts
                 
