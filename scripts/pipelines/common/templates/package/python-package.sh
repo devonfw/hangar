ae250d50a8a56b14419b5d7505105e4ebc9bf212
Abel Antonio Carrion Collado <abcarcol@gmail.com>
2022-11-14 16:41:06 +0100

    Fixed unused variable

--- scripts/pipelines/common/templates/package/python-package.sh
+++ scripts/pipelines/common/templates/package/python-package.sh
@@ -2,3 +2,4 @@
 # We define the tag using the version set in the pyproject.toml
 imageTagFilePath="pyproject.toml"
 tag=$(grep -m 1 version "${imageTagFilePath}" | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3)
+echo "$tag"
