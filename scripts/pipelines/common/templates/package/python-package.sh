5bdb2d36b3515bff11025416ec104b356742e7d0
Abel Antonio Carrion Collado <abcarcol@gmail.com>
2022-11-17 12:53:09 +0100

    Removed useless echo

--- scripts/pipelines/common/templates/package/python-package.sh
+++ scripts/pipelines/common/templates/package/python-package.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # We define the tag using the version set in the pyproject.toml
 imageTagFilePath="pyproject.toml"
-tag=$(grep -m 1 version "${imageTagFilePath}" | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3)
-echo "$tag"
+export tag=$(grep -m 1 version "${imageTagFilePath}" | tr -s ' ' | tr -d '"' | tr -d "'" | cut -d' ' -f3)
