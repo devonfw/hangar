133ab7fe80ae566b149afb6b83edd430f41536cb
Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2022-11-23 19:37:34 +0100

    improve tf script adding quiet command

--- scripts/tf.sh
+++ scripts/tf.sh
@@ -20,22 +20,24 @@ function helpFunction() {
     echo ""
     echo "  apply         Create or update infrastructure"
     echo "  destroy       Destroy previously-created infrastructure"
-    echo "  output        Show output values from your terraform"
+    echo "  output        Show output values from your terraform."
     echo ""
     echo "Common flags:"
     echo ""
     echo "  -s, --state-folder    The folder where you are going to save/import your terraform configuration."
     echo "  -k, --output-key      [ONLY FOR output] The key of the terraform output variable that you want to recover."
+    echo "  -q, --quiet           To not print any command of the script, only the execution of terraform \$command."
     echo "  -h, --help            Get help for commands."
 }
 
 vars=""
-for arg in "${@}"; do
-    case "$arg" in
-        'apply' | 'destroy')             command="$arg --auto-approve"; shift;;
-        'output')                        command="$arg"; shift;;
-        '-s' | '--state-folder')         state="$2"; shift 2;;
-        '-k' | '--output-key')           key="$2"; shift 2;;
+while [[ -n "${@}" ]]; do
+    case "$1" in
+        'apply' | 'destroy')             command="$1 --auto-approve"; shift ;;
+        'output')                        command="$1"; silent="true"; shift ;;
+        '-s' | '--state-folder')         state="$2"; shift 2 ;;
+        '-k' | '--output-key')           key_out="$2"; shift 2 ;;
+        '-q' | '--quiet' )                silent="true"; shift ;;
         'help' | '-h' | '--help')        helpFunction; exit ;;
         *)
             if [[ "$1" == *"="* ]]; then
@@ -49,6 +51,15 @@ for arg in "${@}"; do
     esac
 done
 
+# Function to silent
+silent_cmd() {
+    if $silent; then
+        "$@" > /dev/null
+    else
+        "$@"
+    fi
+}
+
 # Check command provided
 if [[ -z "$command" ]]; then
     echo -e "${red}Error: Missing command, need one of the main commands is mandatory." >&2
@@ -59,55 +70,57 @@ fi
 
 # Import config state
 if [[ -n "$state" ]]; then
-    echo -e "\n${blue}Importing previous state...${white}"
+    silent_cmd echo -e "\n${blue}Importing previous state...${white}" 
     if cp -f "$state/$stateFile" "./$stateFile"; then
-        echo "Previous state successfully imported."
+        silent_cmd echo "Previous state successfully imported."
     else
-        echo "Not previous state found."
+        silent_cmd echo "Not previous state found."
     fi
     if cp -f "$state/$varsFile" "./$varsFile"; then
-        echo "Previous vars successfully imported."
+        silent_cmd echo "Previous vars successfully imported."
     else
-        echo "Not previous vars file found."
+        silent_cmd echo "Not previous vars file found."
     fi
     if cp -f "$state/$outputFile" "./$outputFile"; then
-        echo "Previous output successfully imported."
+        silent_cmd echo "Previous output successfully imported."
     else
-        echo "Not previous output file found."
+        silent_cmd echo "Not previous output file found."
     fi
 fi
 
 # Modify terraform vars
 if [[ -n "$vars" && "$command" != "output" ]]; then
-    echo -e "\n${blue}Modifying terraform variables...${white}"
+    silent_cmd echo -e "\n${blue}Modifying terraform variables...${white}"
     execute="./set-terraform-variables.sh ${vars}"
-    eval "${execute}"
+    silent_cmd eval "${execute}"
 fi
 
 # if command is output, clean state var to not export nothing and if key if exist add it to the command.
 if [[ "$command" == "output" ]]; then
-    if [[ -n "$key" ]]; then
-        command="$command $key"
+    state=""
+    echo "comand output key: $key_out"
+    if [[ -n "$key_out" ]]; then
+        command="$command $key_out"
     fi
 fi
 
 # Execute terraform command
-echo -e "\n${blue}Initializing terraform...${white}"
-terraform init
+silent_cmd echo -e "\n${blue}Initializing terraform...${white}"
+silent_cmd terraform init
 
-echo -e "\n${blue}Executing terraform $command...${white}"
+silent_cmd echo -e "\n${blue}Executing terraform $command...${white}"
 execute="terraform $command"
 eval "${execute}"
 
 # Export config state
 if [[ -n "$state" ]]; then
-    echo -e "\n${blue}Exporting terraform state...${white}"
+    silent_cmd echo -e "\n${blue}Exporting terraform state...${white}"
     mkdir -p "$state"
     terraform output > "$state/$outputFile"
     terraform output -json > "$state/${outputFile}.json"
     if cp -f "./$stateFile" "$state/$stateFile"; then
         if cp -f "./$varsFile" "$state/$varsFile"; then
-            echo -e "${green}Terraform state successfully exported.${white}"
+            silent_cmd echo -e "${green}Terraform state successfully exported.${white}"
         else
             echo -e "${red}Error: Imposible to copy $varsFile file."
             echo -e "${white}"
