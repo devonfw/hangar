cdafc27e0f2fc3b6f6819f5126c4712419bb927b
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-11-24 12:40:35 +0100

    Update scripts/accounts/gcloud/setup-firebase.sh

--- scripts/accounts/gcloud/setup-firebase.sh
+++ scripts/accounts/gcloud/setup-firebase.sh
@@ -70,7 +70,7 @@ ckeckCliInstalled() {
     # Check if GCloud CLI is installed
     if ! [ -x "$(command -v gcloud)" ]; then
         echo -e "${red}Error: GCloud CLI is not installed." >&2
-        echo -ne "${white}"
+        echo -ne "${white}" >&2
         exit 127
     fi
     # Check if Firebase CLI is installed
