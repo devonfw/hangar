fc81a4058ab480953d79fbb1620b6a043d3d1b4c
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-11-24 17:31:02 +0100

    Update quickstart-wayat-backend.sh

--- scripts/quickstart/gcloud/quickstart-wayat-backend.sh
+++ scripts/quickstart/gcloud/quickstart-wayat-backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-FLAGS=$(getopt -a --options w:d:h --long "help,workspace:,directory:" -- "$@")
+FLAGS=$(getopt -a --options w:d:h --long "help,workspace:,directory:,storage-bucket:" -- "$@")
 
 eval set -- "$FLAGS"
 
@@ -10,6 +10,7 @@ while true; do
         -h | --help )             help="true"; shift 1;;
         -w | --workspace )        workspace=$2; shift 2;;
         -d | --directory )        directory=$2; shift 2;;
+        --storage-bucket )        storageBucket=$2; shift 2;;
         --) shift; break;;
     esac
 done
@@ -17,11 +18,12 @@ done
 
 helpFunction() {
     # TODO
-   echo "Creates a new Cloud Run service with a predefined HelloWorld image deployed and retrieves the public URL."
+   echo "Inits the Wayat backend project in the specified workspace folder."
    echo ""
    echo "Flags:"
    echo -e "\t-w, --workspace        [Required] Path for the Takeoff Workspace Project directory"
    echo -e "\t-d, --directory        [Required] Path for the directory of the Backend code"
+   echo -e "\t--storage-bucket       [Required] Storage bucket for storing pictures"
 }
 
 # Colours for the messages.
@@ -44,6 +46,13 @@ checkMandatoryArguments() {
         echo -ne "${white}" >&2
         exit 2
     fi
+    if [ -z "$storageBucket" ];
+    then
+        echo -e "${red}Error: Missing paramenters, --storage-bucket is mandatory." >&2
+        echo -e "${red}Use -h flag to display help." >&2
+        echo -ne "${white}" >&2
+        exit 2
+    fi
 }
 
 # Required CLI check
@@ -59,9 +68,26 @@ ckeckCliInstalled() {
 downloadTemplate() {
     wget -O "$workspace"/wayat-backend-template.zip https://github.com/devonfw/hangar/archive/refs/heads/template/wayat-python-backend.zip
     unzip "$workspace"/wayat-backend-template.zip -d "$workspace/tmp"
-    mv "$workspace"/tmp/hangar-template-wayat-python-backend/ "$directory"
+    shopt -s dotglob
+    mv -f "$workspace"/tmp/hangar-template-wayat-python-backend/* "$directory"
     rm -rf "$workspace/tmp"
     rm "$workspace"/wayat-backend-template.zip
+}
+
+prepareENVFile() {
+    export storageBucket
+    envsubst '$storageBucket' < "$directory/env.template" > "$directory/PROD.env"
+    rm "$directory/env.template"
+    cp "$workspace"/firebase.json "$directory"/firebase.json
+}
+
+commitFiles() {
+    cd "$directory" && git add -A && git commit -m "Init Wayat Backend Code" && git push
+}
+
+addSecrets() {
+    scripts/pipelines/gcloud/add-secret-file.sh -d "$directory" -f "$directory/PROD.env" -r PROD.env -b develop  
+    scripts/pipelines/gcloud/add-secret-file.sh -d "$directory" -f "$directory/firebase.json" -r firebase.json -b develop
 }
 
 #==============================================================
@@ -74,3 +100,9 @@ checkMandatoryArguments
 ckeckCliInstalled
 
 downloadTemplate
+
+prepareENVFile
+
+commitFiles
+
+addSecrets
