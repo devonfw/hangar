8962ce3dd1f3d413a4a040159fedc2af6d8c0ea8
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2022-11-24 12:44:35 +0100

    errors fix

--- scripts/accounts/gcloud/setup-firebase.sh
+++ scripts/accounts/gcloud/setup-firebase.sh
@@ -43,7 +43,7 @@ checkMandatoryArguments() {
     then
         echo -e "${red}Error: Missing paramenters, -n is mandatory." >&2
         echo -e "${red}Use -h flag to display help." >&2
-        echo -ne "${white}"
+        echo -ne "${white}" >&2
         exit 2
     fi
     # Output path check
@@ -51,12 +51,12 @@ checkMandatoryArguments() {
     then
         echo -e "${red}Error: Missing paramenters, -o is mandatory." >&2
         echo -e "${red}Use -h flag to display help." >&2
-        echo -ne "${white}"
+        echo -ne "${white}" >&2
         exit 2
     fi
     if ! test -d "$outputPath"; then
         echo -e "${red}Error: Wrong output path." >&2
-        echo -ne "${white}"
+        echo -ne "${white}" >&2
         exit 2
     fi
     currentPath="$(pwd)"
@@ -70,13 +70,13 @@ ckeckCliInstalled() {
     # Check if GCloud CLI is installed
     if ! [ -x "$(command -v gcloud)" ]; then
         echo -e "${red}Error: GCloud CLI is not installed." >&2
-        echo -ne "${white}"
+        echo -ne "${white}" >&2
         exit 127
     fi
     # Check if Firebase CLI is installed
     if ! [ -x "$(command -v firebase)" ]; then
         echo -e "${red}Error: Firebase CLI is not installed." >&2
-        echo -ne "${white}"
+        echo -ne "${white}" >&2
         exit 128
     fi
 }
@@ -84,8 +84,8 @@ ckeckCliInstalled() {
 checkGcloudProjectName() {
     # Check if exists a Google Cloud project with that project ID. 
     if ! gcloud projects describe "$projectName" 2> /dev/null ; then
-        echo -e "${red}Error: Project $projectName does not exist."
-        echo -ne "${white}"
+        echo -e "${red}Error: Project $projectName does not exist." >&2
+        echo -ne "${white}" >&2
         exit 200
     fi
 }
@@ -93,14 +93,14 @@ checkGcloudProjectName() {
 enableFirebaseServices() {
     echo "Enabling Firebase API..."
     if ! gcloud services enable firebase.googleapis.com --project "$projectName"; then
-        echo -e "${red}Error: Cannot enable Firebase API"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot enable Firebase API" >&2
+        echo -ne "${white}" >&2
         exit 220
     fi
     echo "Enabling Firestore API..."
     if ! gcloud services enable firestore.googleapis.com --project "$projectName"; then
-        echo -e "${red}Error: Cannot enable Firestore API"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot enable Firestore API" >&2
+        echo -ne "${white}" >&2
         exit 221
     fi
 }
@@ -109,14 +109,14 @@ enableMapsAPIs() {
     # Geocoding API service (coordinates to location)
     echo "Enabling Maps APIs:"
     if ! gcloud services enable apikeys.googleapis.com --project "$projectName"; then
-        echo -e "${red}Error: Cannot enable API keys"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot enable API keys" >&2
+        echo -ne "${white}" >&2
         exit 216
     fi
     echo "Enabling Geocoding Backend..."
     if ! gcloud services enable geocoding-backend.googleapis.com --project "$projectName"; then
-        echo -e "${red}Error: Cannot enable Geocoding Backend API"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot enable Geocoding Backend API" >&2
+        echo -ne "${white}" >&2
         exit 224
     fi
     # Android MAPS API service
@@ -124,8 +124,8 @@ enableMapsAPIs() {
     then
         echo "Enabling Maps Android Backend..."
         if ! gcloud services enable maps-android-backend.googleapis.com --project "$projectName"; then
-            echo -e "${red}Error: Cannot enable Maps Android Backend API"
-            echo -ne "${white}"
+            echo -e "${red}Error: Cannot enable Maps Android Backend API" >&2
+            echo -ne "${white}" >&2
             exit 222
         fi
     fi
@@ -134,8 +134,8 @@ enableMapsAPIs() {
     then
         echo "Enabling Maps IOS Backend..."
         if ! gcloud services enable maps-ios-backend.googleapis.com --project "$projectName"; then
-            echo -e "${red}Error: Cannot enable Maps IOS Backend API"
-            echo -ne "${white}"
+            echo -e "${red}Error: Cannot enable Maps IOS Backend API" >&2
+            echo -ne "${white}" >&2
             exit 223
         fi
     fi
@@ -144,16 +144,16 @@ enableMapsAPIs() {
     then
         echo "Enabling Maps WEB..."
         if ! gcloud services enable --project "$projectName" maps-backend.googleapis.com ; then
-            echo -e "${red}Error: Cannot enable Maps Backend API"
-            echo -ne "${white}"
+            echo -e "${red}Error: Cannot enable Maps Backend API" >&2
+            echo -ne "${white}" >&2
             exit 216
         fi
     fi
     # Static MAPS API service (coordinates to screenshot)
     echo "Enabling Static Maps Backend..."
     if ! gcloud services enable static-maps-backend.googleapis.com --project "$projectName"; then
-        echo -e "${red}Error: Cannot enable Static Maps Backend API"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot enable Static Maps Backend API" >&2
+        echo -ne "${white}" >&2
         exit 225
     fi
 }
@@ -162,8 +162,8 @@ addFirebaseToGcloudProject() {
     echo "Adding Firebase to Project..."
     if [[ $( firebase projects:list | grep "$projectName" ) == "" ]] ; then
         if ! firebase projects:addfirebase "$projectName"; then
-            echo -e "${red}Error: Cannot add Firebase to project."
-            echo -ne "${white}"
+            echo -e "${red}Error: Cannot add Firebase to project." >&2
+            echo -ne "${white}" >&2
             exit 201
         fi
     else
@@ -180,8 +180,8 @@ createFirestoreDB() {
         echo -e "App Engine already created"
     fi
     if ! gcloud firestore databases create --project $projectName --region=$firestoreRegion; then
-        echo -e "${red}Error: Cannot Create Firestore Database"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot Create Firestore Database" >&2
+        echo -ne "${white}" >&2
         exit 230
     fi
 }
@@ -190,8 +190,8 @@ createFirebaseSDKAccount() {
     echo "Creating Firebase SDK Service Account..."
     service_email=$(gcloud iam service-accounts list | grep firebase-adminsdk | tr -s ' ' | cut -d ' ' -f2)
     if ! gcloud iam service-accounts keys create $outputPath"/firebase.json" --iam-account "$service_email" --project "$projectName"; then
-        echo -e "${red}Error: Cannot create Firebase Service Account"
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot create Firebase Service Account" >&2
+        echo -ne "${white}" >&2
         exit 240
     fi
 }
@@ -216,13 +216,13 @@ registerShaKeys() {
     sha1Key=$(echo -e "android\n" | keytool -list -v -alias upload -keystore $outputPath"/keystore.jks" 2> /dev/null | grep SHA1 -m 1 | cut -d' ' -f3)
     sha256Key=$(echo -e "android\n" | keytool -list -v -alias upload -keystore $outputPath"/keystore.jks" 2> /dev/null | grep SHA256 -m 1 | cut -d' ' -f3)
     if ! firebase apps:android:sha:create $appId $sha1Key --project ${projectName} ; then
-        echo -e "${red}Error: Cannot add SHA1 key."
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot add SHA1 key." >&2
+        echo -ne "${white}" >&2
         exit 303
     fi
     if ! firebase apps:android:sha:create $appId $sha256Key --project ${projectName} ; then
-        echo -e "${red}Error: Cannot add SHA256 key."
-        echo -ne "${white}"
+        echo -e "${red}Error: Cannot add SHA256 key." >&2
+        echo -ne "${white}" >&2
         exit 304
     fi
 }
@@ -241,20 +241,20 @@ createPlatformApps() {
         command=$base_create_app_command" --package-name ${packageName} ANDROID ${projectName}_android"
         if ! eval "$command"; then
             echo -e "${red}Error while creating Android APP." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 250
         fi
         setupAndroidKeystore
         if ! gcloud services enable apikeys.googleapis.com --project "${projectName}" ; then
             echo -e "${red}Error while enabling API keys API." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 250
         fi
         registerShaKeys
         command=$base_sdkconfig_command" --out ${outputPath}/google-services.json ANDROID"
         if ! eval "$command"; then
             echo -e "${red}Error while exporting SDK Android Config." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 251
         fi
     fi
@@ -265,13 +265,13 @@ createPlatformApps() {
         command=$base_create_app_command" --bundle-id ${packageName} --app-store-id \"\" IOS ${projectName}_ios"
         if ! eval "$command"; then
             echo -e "${red}Error while creating IOS APP." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 252
         fi
         command=$base_sdkconfig_command" --out ${outputPath}/GoogleService-Info.plist IOS"
         if ! eval "$command"; then
             echo -e "${red}Error while exporting SDK IOS Config." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 253
         fi
     fi
@@ -282,13 +282,13 @@ createPlatformApps() {
         command=$base_create_app_command" WEB ${projectName}_web"
         if ! eval "$command"; then
             echo -e "${red}Error while creating WEB APP." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 254
         fi
         command=$base_sdkconfig_command" --out ${outputPath}/webconfig.json WEB"
         if ! eval "$command"; then
             echo -e "${red}Error while exporting SDK WEB Config." >&2
-            echo -ne "${white}"
+            echo -ne "${white}" >&2
             exit 255
         fi
     fi
