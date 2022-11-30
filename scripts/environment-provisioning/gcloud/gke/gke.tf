65ebd903b9c5c41b18d1c26b581df79b79211291
Miguel A. Cerver <miguel-angel.cerver-copovi@capgemini.com>
2022-11-30 15:01:35 +0100

    Suggestions applied

--- scripts/environment-provisioning/gcloud/gke/gke.tf
+++ scripts/environment-provisioning/gcloud/gke/gke.tf
@@ -53,24 +53,3 @@ resource "google_container_node_pool" "primary_nodes" {
     }
   }
 }
-
-
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
-
-# provider "kubernetes" {
-#   load_config_file = "false"
-
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
-
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
-
--- scripts/pipelines/gcloud/pipeline_generator.sh
+++ scripts/pipelines/gcloud/pipeline_generator.sh
@@ -33,6 +33,7 @@ while true; do
         --aws-access-key)         awsAccessKey="$2"; shift 2;;
         --aws-secret-access-key)  awsSecretAccessKey="$2"; shift 2;;
         --aws-region)             awsRegion="$2"; shift 2;;
+        --gcloud-region)          gcloudRegion="$2"; shift 2;;
         -h | --help)              help="true"; shift 1;;
         -m | --machine-type)      machineType="$2"; shift 2;;
         --language-version)       languageVersion="$2"; shift 2;;
