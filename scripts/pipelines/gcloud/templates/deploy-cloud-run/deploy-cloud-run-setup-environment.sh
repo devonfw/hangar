1865de20053c3e58de4ef365384eb98780380808
Timothe <timothepaty@gmail.com>
2022-11-30 14:22:40 +0100

    resolving commentaries

--- scripts/pipelines/gcloud/templates/deploy-cloud-run/deploy-cloud-run-setup-environment.sh
+++ scripts/pipelines/gcloud/templates/deploy-cloud-run/deploy-cloud-run-setup-environment.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
-registry="$1"
+image_name="$1"
+# We get the name of the registry from the full image name
+firstPartImage=$(echo $image_name | cut -d'/' -f1)
+echo $firstPartImage | grep "\." > /dev/null && REGISTRY=$firstPartImage || REGISTRY="docker.io"
 
 #
-if ! { [[ "$registry" =~ .*docker.pkg.dev.* ]] || [[ "$registry" =~ .*gcr.io.* ]] ; }
+if ! { [[ "$REGISTRY" =~ .*docker.pkg.dev.* ]] || [[ "$REGISTRY" =~ .*gcr.io.* ]] ; }
 then
   apt-get update && apt-get install docker.io -y
-  if [[ "$registry" =~ .*amazonaws.com.* ]]
+  if [[ "$REGISTRY" =~ .*amazonaws.com.* ]]
   then
 	  apt-get install unzip
 	  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
