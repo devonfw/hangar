2fe65b0a9ad0943c6fad239fe89a97f5fcc1ebb7
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2022-12-02 11:51:24 +0100

    Update setup-firebase.sh

--- scripts/accounts/gcloud/setup-firebase.sh
+++ scripts/accounts/gcloud/setup-firebase.sh
@@ -233,7 +233,7 @@ createPlatformApps() {
     # Remove '-' character
     packageName="com.takeoff.${projectName//-/}"
     # Remove '_' character
-    packageName="com.takeoff.${packageName//_/}"
+    packageName="${packageName//_/}"
     # ANDROID setup:
     if [[ "$setupAndroid" == "true" ]]
     then
