d30d4859d036f93d4cd37f97efca310e80285f17
Fabián Scherle <101186527+capf4bbo@users.noreply.github.com>
2022-12-09 10:51:50 +0100

    Initial fix for flutter platform !int

--- scripts/pipelines/common/templates/package/flutter-package.sh
+++ scripts/pipelines/common/templates/package/flutter-package.sh
@@ -4,14 +4,14 @@ export tag=$(grep -m 1 version pubspec.yaml | tr -s ' ' | tr -d ':' | cut -d' ' 
 # Colours for the messages.
 red='\e[0;31m'
 
-if [[ ${FLUTTER_PLATFORM} == "android" ]]; then
-    flutter pub get
+flutter pub get
+if [[ -n ${ANDROID_PLATFORM} && ${ANDROID_PLATFORM} == "true" ]]; then
     flutter build apk --release
     #Install gcloud CLI
     apt-get install apt-transport-https ca-certificates gnupg -y | echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-cli -y
     gcloud storage cp build/app/outputs/flutter-apk/app-release.apk gs://"${PROJECT_ID}"-apk/app-release-"${SHORT_SHA}".apk
-elif [[ ${FLUTTER_PLATFORM} == "web" ]]; then
-    flutter pub get
+fi
+if [[ -n ${WEB_PLATFORM} && ${WEB_PLATFORM} == "true" ]]; then
     if [[ ${FLUTTER_WEB_RENDERER} == "" ]]; then
         flutter build web --release --web-renderer auto --dart-define=BROWSER_IMAGE_DECODING_ENABLED=false
         # Check if the web renderer is supported
