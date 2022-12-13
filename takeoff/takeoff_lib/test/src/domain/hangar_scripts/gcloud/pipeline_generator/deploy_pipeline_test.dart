5a11120105dc8fc81fc614fa5e9e38f8a3cca092
Josué Ausejo Hernández <josue.ausejo-hernandez@capgemini.com>
2022-12-13 09:19:39 +0100

    deploy pipeline test WIP

--- takeoff/takeoff_lib/test/src/domain/hangar_scripts/gcloud/pipeline_generator/deploy_pipeline_test.dart
+++ takeoff/takeoff_lib/test/src/domain/hangar_scripts/gcloud/pipeline_generator/deploy_pipeline_test.dart
@@ -1,17 +1,7 @@
-import 'package:takeoff_lib/src/domain/hangar_scripts/common/language/language.dart';
-import 'package:takeoff_lib/src/domain/hangar_scripts/gcloud/common/machine_type.dart';
-import 'package:takeoff_lib/src/domain/hangar_scripts/gcloud/pipeline_generator/build_pipeline.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test("BuildPipelineGCloud to command generates the arguments correctly", () {
-    BuildPipelineGCloud buildPipelineGCloud = BuildPipelineGCloud(configFile: "configFile", pipelineName: "pipelineName", language: Language.flutter, localDirectory: "localDirectory");
-    expect(buildPipelineGCloud.toCommand(), ['/scripts/pipelines/gcloud/pipeline_generator.sh','-c','configFile','-n','pipelineName','--local-directory','localDirectory','-l','flutter']);
-  });
-
-  test("BuildPipelineGCloud to command with all the parameters generates the arguments correctly", () {
-    BuildPipelineGCloud buildPipelineGCloud = BuildPipelineGCloud(configFile: "configFile", pipelineName: "pipelineName", language: Language.flutter, localDirectory: "localDirectory", registryLocation: "registryLocation", targetDirectory: "targetDirectory", machineType: MachineType.E2_HIGHCPU_32);
-    expect(buildPipelineGCloud.toCommand(), ['/scripts/pipelines/gcloud/pipeline_generator.sh','-c','configFile','-n','pipelineName','--local-directory','localDirectory','-l','flutter','--registry-location','registryLocation','-t','targetDirectory','-m','E2_HIGHCPU_32']);
-  }); 
+  // test("BuildPipelineGCloud to command generates the arguments correctly", () {
+  // });
 
 }
