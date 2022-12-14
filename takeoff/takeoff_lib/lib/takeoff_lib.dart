c3d9d2d503c4c5dbfbb2ce2a9c014809bda634fa
Serhii Bets <serhii.bets@capgemini.com>
2022-12-14 17:00:41 +0100

    ignore

--- takeoff/takeoff_lib/lib/takeoff_lib.dart
+++ takeoff/takeoff_lib/lib/takeoff_lib.dart
@@ -13,7 +13,5 @@ export 'src/domain/gui_message/gui_message.dart';
 export 'src/domain/gui_message/input_type.dart';
 export 'src/domain/gui_message/message_type.dart';
 export 'src/controllers/cloud/common/hangar/project/create_project_exception.dart';
-export 'src/utils/url_launcher/resource_type.dart';
-
 
 // TODO: Export any libraries intended for clients of this package.
