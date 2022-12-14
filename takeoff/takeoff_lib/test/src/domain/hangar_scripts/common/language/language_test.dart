535ad0fb08dbbde50e3d14d31e0e105b5db97b2a
airherna <airam.hernandez-rocha@capgemini.com>
2022-12-14 09:05:01 +0100

    merged takeoff develop and fixed imports

--- takeoff/takeoff_lib/test/src/domain/hangar_scripts/common/language/language_test.dart
+++ takeoff/takeoff_lib/test/src/domain/hangar_scripts/common/language/language_test.dart
@@ -1,4 +1,4 @@
-import 'package:takeoff_lib/src/domain/hangar_scripts/common/language/language.dart';
+import 'package:takeoff_lib/src/domain/language.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -6,37 +6,37 @@ void main() {
     Language language = Language.fromString('quarkus');
     expect(language, Language.quarkus);
   });
-  
+
   test("Language quarkus-jvm returns the correct language type", () {
     Language language = Language.fromString('quarkus-jvm');
     expect(language, Language.quarkusJVM);
   });
-  
+
   test("Language node returns the correct language type", () {
     Language language = Language.fromString('node');
     expect(language, Language.node);
   });
-  
+
   test("Language angular returns the correct language type", () {
     Language language = Language.fromString('angular');
     expect(language, Language.angular);
   });
-  
+
   test("Language python returns the correct language type", () {
     Language language = Language.fromString('python');
     expect(language, Language.python);
   });
-  
+
   test("Language flutter returns the correct language type", () {
     Language language = Language.fromString('flutter');
     expect(language, Language.flutter);
   });
-  
+
   test("Language empty returns the correct language type", () {
     Language language = Language.fromString('');
     expect(language, Language.none);
   });
-  
+
   test("Language unknown returns the correct language type", () {
     Language language = Language.fromString('unknown');
     expect(language, Language.none);
