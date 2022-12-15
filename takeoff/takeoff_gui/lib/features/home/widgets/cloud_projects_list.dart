ad3351bde5a070d9b90a20cf1a0b598a61f123ba
Josué Ausejo Hernández <josue.ausejo-hernandez@capgemini.com>
2022-12-15 13:21:10 +0100

    WIP

--- takeoff/takeoff_gui/lib/features/home/widgets/cloud_projects_list.dart
+++ takeoff/takeoff_gui/lib/features/home/widgets/cloud_projects_list.dart
@@ -20,38 +20,36 @@ class CloudProjectsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: CloudProviderHeader(
-                name: name,
-                authenticateCallback: authenticateCallback,
-                authAccount: authAccount,
-                logOutCallback: logOutCallback,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: CloudProviderHeader(
+              name: name,
+              authenticateCallback: authenticateCallback,
+              authAccount: authAccount,
+              logOutCallback: logOutCallback,
+            ),
+          ),
+          const SizedBox(height: 10),
+          // List of projects
+          if (authAccount.isNotEmpty && projects.isNotEmpty)
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                itemCount: projects.length,
+                itemBuilder: (context, index) {
+                  return CloudProjectItem(
+                    project: projects[index],
+                  );
+                },
+                scrollDirection: Axis.horizontal,
               ),
             ),
-            const SizedBox(height: 10),
-            // List of projects
-            if (authAccount.isNotEmpty && projects.isNotEmpty)
-              SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  itemCount: projects.length,
-                  itemBuilder: (context, index) {
-                    return CloudProjectItem(
-                      project: projects[index],
-                    );
-                  },
-                  scrollDirection: Axis.horizontal,
-                ),
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
