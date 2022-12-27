8f3bf42c943138a7ebf3a0a4bab95a9f64025e10
Jorge Dacal Cantos <jorge.dacal-cantos@capgemini.com>
2022-12-27 10:24:44 +0100

    add script to change wiki files

---
+++ .github/workflows/scripts/rename-documentation-for-wiki.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+docBase="."
+docType=".asciidoc"
+res="test-wiki"
+oldPath=()
+
+function renameFiles {
+  for input in $basePath/*
+  do
+    echo "readin... $input"
+    # When folder call rename again with new base
+    if [[ -d "$input" ]]; then
+      basePath="$input"
+      renameFiles
+    # When doctype do things
+    elif [[ "$input" == *"$docType" ]]; then
+      name=${input//"./"/}
+      base="${name%/*$docType}"
+      name=${name//"/"/"-"}
+      base=${base//"/"/"-"}
+      if [[ "$base" == *"$docType" ]]; then
+        base=""
+      fi
+      ## Change internal links to new base path
+      sed -i "s/xref:/xref:$base-/g" $input
+      ## Remove .asciidoc for links
+      # sed -i "s/.asciidoc//g" $input
+      ## Move and rename file
+      mv $input $docBase/$name
+    fi
+  done
+}
+
+# 1. Remove src
+rm -rf ./src
+
+# 2. call to renameFiles
+basePath="$docBase"
+renameFiles
