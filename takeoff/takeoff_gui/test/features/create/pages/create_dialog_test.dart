bae7addcd371095cae0461f9697d41bb7b034261
Serhii Bets <serhii.bets@capgemini.com>
2022-12-29 15:48:24 +0100

    Ignore

--- takeoff/takeoff_gui/test/features/create/pages/create_dialog_test.dart
+++ takeoff/takeoff_gui/test/features/create/pages/create_dialog_test.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:takeoff_gui/common/custom_button.dart';
 import 'package:takeoff_gui/features/create/controllers/create_controller.dart';
 import 'package:takeoff_gui/features/create/controllers/project_form_controllers/google_form_controller.dart';
 import 'package:takeoff_gui/features/create/pages/create_dialog.dart';
@@ -11,7 +9,7 @@ import 'package:takeoff_gui/features/create/utils/languages_versions.dart';
 import 'package:takeoff_lib/takeoff_lib.dart';
 
 import '../../../common/test_widget.dart';
-import '../create_dialog_test.mocks.dart';
+import 'create_dialog_test.mocks.dart';
 
 @GenerateNiceMocks(
     [MockSpec<CreateController>(), MockSpec<GoogleFormController>()])
@@ -25,19 +23,19 @@ void main() async {
     GetIt.I.registerSingleton<GoogleFormController>(mockGoogleFormController);
   });
 
-  testWidgets('Widget has correct text', (tester) async {
+  testWidgets('Widget has text', (tester) async {
     when(mockGoogleFormController.projectName).thenReturn("TestProjectName");
     when(mockGoogleFormController.billingAccount)
         .thenReturn("0000-0000-0000-0000");
     when(mockGoogleFormController.region).thenReturn("europe-west2");
 
     when(mockProjectsController.backendLanguage).thenReturn(Language.python);
-    when(mockProjectsController.backendVersion)
-        .thenReturn(LanguagesVersions.backendLanguages.first.name);
+    when(mockProjectsController.backendVersion).thenReturn(LanguagesVersions
+        .versionsLanguages[LanguagesVersions.backendLanguages.first]!.first);
 
     when(mockProjectsController.frontendLanguage).thenReturn(Language.flutter);
-    when(mockProjectsController.frontendVersion)
-        .thenReturn(LanguagesVersions.frontendLanguages.first.name);
+    when(mockProjectsController.frontendVersion).thenReturn(LanguagesVersions
+        .versionsLanguages[LanguagesVersions.frontendLanguages.first]!.first);
 
     await tester.pumpWidget(TestWidget(child: CreateDialog()));
     expect(find.text("Create a project"), findsOneWidget);
