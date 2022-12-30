901a131403413f52d848077057d824e1f02ab4fb
Serhii Bets <serhii.bets@capgemini.com>
2022-12-30 13:41:56 +0100

    Add tests to monitor_dialog

--- takeoff/takeoff_gui/test/common/monitor/pages/monitor_dialog_test.dart
+++ takeoff/takeoff_gui/test/common/monitor/pages/monitor_dialog_test.dart
@@ -1,40 +1,84 @@
 //@GenerateNiceMocks([])
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart' as mobx;
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:takeoff_gui/common/custom_button.dart';
 import 'package:takeoff_gui/common/monitor/controllers/monitor_controller.dart';
 import 'package:takeoff_gui/common/monitor/pages/monitor_dialog.dart';
+import 'package:takeoff_gui/features/create/utils/create_message.dart';
+import 'package:takeoff_gui/features/create/utils/type_message.dart';
+import 'package:takeoff_gui/features/home/controllers/projects_controller.dart';
+import 'package:takeoff_lib/takeoff_lib.dart';
 
 import '../../test_widget.dart';
 import 'monitor_dialog_test.mocks.dart';
 
-@GenerateNiceMocks([
-  MockSpec<MonitorController>(),
-])
+@GenerateNiceMocks(
+    [MockSpec<MonitorController>(), MockSpec<ProjectsController>()])
 void main() async {
   final MockMonitorController mockMonitorController = MockMonitorController();
+  final MockProjectsController mockProjectsController =
+      MockProjectsController();
+  late MonitorDialog monitorDialog;
 
   setUpAll(() async {
     GetIt.I.registerSingleton<MonitorController>(mockMonitorController);
+    GetIt.I.registerSingleton<ProjectsController>(mockProjectsController);
+    monitorDialog = MonitorDialog();
   });
 
-  // testWidgets('Widget has correct data', (tester) async {
-  //   await tester.pumpWidget(TestWidget(child: MonitorDialog()));
+  testWidgets('Widget has correct data when Project is created and success',
+      (tester) async {
+    CreateMessage message =
+        CreateMessage(TypeMessage.success, "Test message - isSuccess = true");
 
-  //   when(mockMonitorController.hasFinished).thenReturn(true);
-  //   expect(find.text("Project creation finished"), findsOneWidget);
-  //   expect(find.byType(CustomButton), findsOneWidget);
-  //   expect(find.byIcon(Icons.browser_updated_outlined), findsOneWidget);
-  //   when(mockMonitorController.isSuccess).thenReturn(true);
-  //   expect(find.text("Open project"), findsOneWidget);
-  //   when(mockMonitorController.isSuccess).thenReturn(false);
-  //   expect(find.text("Close"), findsOneWidget);
+    when(mockMonitorController.outputChannel).thenReturn(StreamController());
+    when(mockMonitorController.steps)
+        .thenReturn(mobx.ObservableList.of([message]));
+    when(mockMonitorController.hasFinished).thenReturn(true);
+    when(mockMonitorController.isSuccess).thenReturn(true);
+    await tester.pumpWidget(TestWidget(child: MonitorDialog()));
 
-  //   when(mockMonitorController.hasFinished).thenReturn(false);
-  //   expect(find.text("Creating project..."), findsOneWidget);
-  //   expect(find.byType(Container), findsOneWidget);
-  // });
+    expect(find.text("Project creation finished"), findsOneWidget);
+    expect(find.byType(CustomButton), findsOneWidget);
+    expect(find.byIcon(Icons.browser_updated_outlined), findsOneWidget);
+    expect(find.text("Open project"), findsOneWidget);
+  });
+
+  testWidgets('Widget has correct data when Project is created and not success',
+      (tester) async {
+    CreateMessage message =
+        CreateMessage(TypeMessage.success, "Test message - isSuccess = false");
+
+    when(mockMonitorController.outputChannel).thenReturn(StreamController());
+    when(mockMonitorController.steps)
+        .thenReturn(mobx.ObservableList.of([message]));
+    when(mockMonitorController.hasFinished).thenReturn(true);
+    when(mockMonitorController.isSuccess).thenReturn(false);
+    await tester.pumpWidget(TestWidget(child: MonitorDialog()));
+
+    expect(find.text("Project creation finished"), findsOneWidget);
+    expect(find.byType(CustomButton), findsOneWidget);
+    expect(find.byIcon(Icons.browser_updated_outlined), findsOneWidget);
+    expect(find.text("Close"), findsOneWidget);
+  });
+
+  testWidgets('Widget has correct data when Creating project...',
+      (tester) async {
+    CreateMessage message =
+        CreateMessage(TypeMessage.success, "Test message - success");
+
+    when(mockMonitorController.outputChannel).thenReturn(StreamController());
+    when(mockMonitorController.steps)
+        .thenReturn(mobx.ObservableList.of([message]));
+    when(mockMonitorController.hasFinished).thenReturn(false);
+    await tester.pumpWidget(TestWidget(child: MonitorDialog()));
+
+    expect(find.text("Creating project..."), findsOneWidget);
+  });
 }
