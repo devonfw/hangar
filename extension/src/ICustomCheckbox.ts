79dcd9c9fd04d980998a5a0c94c2c4cbcdcbb1de
hesantan-sudo <hector-antonio.santana-beneyto@capgemini.com>
2023-11-24 10:24:12 +0100

    Update: Version and class comment

--- extension/src/ICustomCheckbox.ts
+++ extension/src/ICustomCheckbox.ts
@@ -3,11 +3,14 @@
  * 
  * This interface defines the structure of a custom checkbox, which includes a unique identifier and a label.
  * 
+ * @param id - 🛑 IMPORTANT: Use same script filename for the id 🛑
+ * @param label - Checkbox text
+ * 
  * @example
- * const customCheckbox: ICustomCheckbox = { id: 'exampleId', label: 'Example' };
+ * const customCheckbox: ICustomCheckbox = { id: 'create-repo.sh', label: '🆙 Create repo' };
  * 
  * @author ADCenter Spain - DevOn Hangar Team
- * @version 1.0.0
+ * @version 1.1.0
  */
 export interface ICustomCheckbox {
     id: string;
