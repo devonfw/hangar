3f0f053619fa6a3e203adc3eba3ffd7f120ebf7a
victordrose <victormalfocea@me.com>
2023-12-05 12:46:31 +0100

    cambiado nombre a ci

--- scripts/pipelines/common/templates/cicd/quarkus-allInOne.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mvn package -B -Pnative
-
-mvn test -B -Dmaven.install.skip=true -Pnative
-
-# Quality
-mvn sonar:sonar -B -Dsonar.host.url="$SONAR_URL" -Dsonar.login="$SONAR_TOKEN" -Dsonar.java.binaries=$PROJECT_PATH/target/classes
