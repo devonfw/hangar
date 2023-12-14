5e3b855a5216ffc968e49b30fb4aeff2a30f30c6
victordrose <victormalfocea@me.com>
2023-12-14 11:49:23 +0100

    cambiada función add_jacoco_dependencies

--- scripts/pipelines/azure-devops/pipeline_generator.sh
+++ scripts/pipelines/azure-devops/pipeline_generator.sh
@@ -142,12 +142,32 @@ function add_jacoco_dependency {
     if [[ $language == "quarkus"* ]]
     then
         echo "Adding JaCoCo dependency to pom.xml..."
-        sed -i '/<\/dependencies>/i\
-        <dependency>\
-            <groupId>org.jacoco</groupId>\
-            <artifactId>jacoco-maven-plugin</artifactId>\
-            <version>jacoco.3.2.0</version>\
-        </dependency>' $localDirectory/pom.xml
+        python3 -c 
+import xml.etree.ElementTree as ET
+
+# Parse the pom.xml file
+tree = ET.parse('$localDirectory/pom.xml')
+root = tree.getroot()
+
+# Define the namespace
+namespaces = {'xmlns': 'http://maven.apache.org/POM/4.0.0'}
+ET.register_namespace('', namespaces['xmlns'])
+
+# Find the <dependencies> element
+dependencies = root.find('xmlns:dependencies', namespaces)
+
+# Create the JaCoCo dependency element
+dependency = ET.SubElement(dependencies, 'dependency')
+groupId = ET.SubElement(dependency, 'groupId')
+groupId.text = 'org.jacoco'
+artifactId = ET.SubElement(dependency, 'artifactId')
+artifactId.text = 'jacoco-maven-plugin'
+version = ET.SubElement(dependency, 'version')
+version.text = '3.2.0'  # Replace with the actual version of JaCoCo
+
+# Write the modified XML back to the pom.xml file
+tree.write('$localDirectory/pom.xml', encoding='utf-8', xml_declaration=True)
+
     fi
 }
 
